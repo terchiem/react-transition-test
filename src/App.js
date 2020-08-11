5b1b263eb1f09b8783caaa830682553c8d42bc35
Terry Chiem <terrychiem@gmail.com>
2020-08-10 22:56:10 -0700

    Add controls to load/unload components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+
 import PhotoList from './PhotoList';
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com/photos';
+
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [temp, setTemp] = useState(null);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await axios.get(BASE_URL);
+      response.data.length = 50;
+      setPhotos(response.data);
+    }
+    loadData();
+  }, []);
+
+  function clear() {
+    setTemp(photos);
+    setPhotos([]);
+  }
+
+  function load() {
+    setPhotos(temp);
+  }
+
   return (
     <div className="App">
-      <PhotoList />
+      <button onClick={clear}>Clear</button>
+      <button onClick={load}>Load</button>
+
+      <PhotoList photos={photos} />
     </div>
   );
 }
--- src/PhotoList.js
+++ src/PhotoList.js
@@ -1,23 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Photo from './Photo';
 import './PhotoList.css';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/photos';
 
-function PhotoList() {
-  const [photos, setPhotos] = useState([]);
+function PhotoList({ photos }) {
 
-
-  useEffect(() => {
-    async function loadData() {
-      const response = await axios.get(BASE_URL);
-      response.data.length = 50;
-      setPhotos(response.data);
-    }
-    loadData();
-
-  }, []);
 
   return (
     <div className="PhotoList">
